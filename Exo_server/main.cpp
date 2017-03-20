702d01eb51abbbebf59b3cf5b8f19fc5743bdbc7
otzhora <y_rogachev@inbox.ru>
2017-03-20 18:57:02 +0300

    mda

--- Exo_server/main.cpp
+++ Exo_server/main.cpp
@@ -6,6 +6,7 @@
 
 #include <netinet/in.h>
 #include <unistd.h>
+#include <netdb.h>
 
 using namespace std;
 
@@ -23,9 +24,9 @@ int main(int argc, char** argv) {
 
     sa.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
     sa.sin_family = AF_INET;
-    sa.sin_port = htons(12346);
+    sa.sin_port = htons(8001);
 
-    if(bind(s, (const struct sockaddr*)&sa, sizeof(sa)) == -1)
+    if(bind(s, (struct sockaddr*)&sa, sizeof(sa)) == -1)
     {
         cout << "bind err" << endl;
         return 1;
@@ -34,15 +35,26 @@ int main(int argc, char** argv) {
     listen(s, SOMAXCONN);
 
     int s1;
-    char buf[5];
-    while(s1 = accept(s, 0, 0))
-    {
-        recv(s1, buf, 5, MSG_NOSIGNAL);
-        cout << buf << endl;
-        send(s1, buf, 5, MSG_NOSIGNAL);
-        close(s1);
-    }
+    char buf[128];
 
+    do
+    {
+        int a, b;
+        s1 = accept(s, 0, 0);
+        cout << "accepted " << endl;
+        recv(s1, buf, 4, MSG_NOSIGNAL);
+        a = atoi(buf);
+        cout << buf << endl;
+        recv(s1, buf, 4, MSG_NOSIGNAL);
+        b = atoi(buf);
+        cout << b << endl;
+        int ans = a + b;
+        string msg = to_string(ans);
+        cout << send(s1, (char*)(msg.c_str()), 4, MSG_NOSIGNAL) << endl;
+        cout << "sended" << ans <<  endl;
+        close(s1);
+    }while(s1 != -1);
+    close(s);
 
     return 0;
 }
