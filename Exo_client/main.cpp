09b6b35df1148075db21d18b23a44a5c00f18a13
otzhora <y_rogachev@inbox.ru>
2017-03-20 18:57:48 +0300

    mda

--- Exo_client/main.cpp
+++ Exo_client/main.cpp
@@ -8,24 +8,43 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
+#include <string>
+
 using namespace std;
 
 int main() {
     int s = socket(AF_INET, SOCK_STREAM, 0);
 
+    string port, ip_addr;
+    cout << "input port" << endl;
+   // cin >> port;
     struct sockaddr_in sa;
-    sa.sin_port = htons(12346);
+    sa.sin_port = htons(8001);
     sa.sin_family = AF_INET;
     sa.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    cout << INADDR_LOOPBACK << endl;
+    if (connect(s, (const sockaddr*)&sa, sizeof(sa)) == -1)
+    {
+        cout << "connect error" << endl;
+        return 1;
+    }
+    cout << "connection success!" << endl;
+    cout << "input two number to sum: ";
 
-    connect(s, (const sockaddr*)&sa, sizeof(sa));
+    char a[5], b[5]         ;
 
-    char buf[5];
-    cin >> buf;
+    cin >> a >> b;
+    int bytes;
 
-    send(s, buf, 5, MSG_NOSIGNAL);
-    recv(s, buf, 5, MSG_NOSIGNAL);
-    cout << buf << endl;
+    bytes = send(s, a, 4, MSG_NOSIGNAL);
+    cout << bytes;
+    bytes = send(s, b, 4, MSG_NOSIGNAL);
+    cout << bytes;
+    cout << "sended" << endl;
+
+    char ans[5];
+    recv(s, ans, 4, MSG_NOSIGNAL);
+    cout << "answer: " << atoi(ans) << endl;
     shutdown(s, SHUT_RDWR);
     close(s);
     return 0;
